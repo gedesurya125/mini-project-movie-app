6d536921dda0f288d7419578f6bd932eb336f2e0
gedesurya125 <gedesurya125@gmail.com>
2021-07-31 01:10:56 +0800

    moving Header and Footer to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { getMoviesAction } from './redux/actions/moviesAction'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import DetailsPage from './pages/DetailsPage';
 
@@ -12,10 +14,12 @@ const App = () => {
   }, [dispatch])
   return (
     <Router>
+      <Header/>
       <Switch>
         <Route exact path="/" component={()=>(<HomePage />)} /> {/**HomePage */}
         <Route path="/details/:movie_id" component={() => (<DetailsPage />)} />
       </Switch>
+      <Footer/>
     </Router>
   )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,15 @@
 import { Button } from "@material-ui/core";
 import React from "react";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+
 
 const HomePage = () => {
   return (
     <div>
-      <Header />
+
       <Button variant="contained" color="primary">
         Click Me
       </Button>
-      <Footer />
+
     </div>
   );
 };
