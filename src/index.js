1c4c7716de0b334efa1209e8d37a76435b88326b
FADHILLA DWI PRAMESWARY RAYES <fadhilla.dwi.p.r@gmail.com>
2021-07-30 17:10:57 +0700

    footer component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import reactDom from 'react-dom'
-import App from './App';
-import store from './redux/store';
-import { Provider } from 'react-redux';
-import { CssBaseline } from '@material-ui/core';
-import { ThemeProvider, createTheme } from '@material-ui/core/styles';
-const rootTheme = createTheme({
-
-})
+import React from "react";
+import reactDom from "react-dom";
+import App from "./App";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
+import Footer from "./components/Footer";
+const rootTheme = createTheme({});
 
 reactDom.render(
   <ThemeProvider theme={rootTheme}>
@@ -15,8 +14,6 @@ reactDom.render(
       <CssBaseline />
       <App />
     </Provider>
-  </ThemeProvider>
-  , document.getElementById('root'));
-
-
-
+  </ThemeProvider>,
+  document.getElementById("root")
+);
